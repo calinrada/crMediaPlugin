6334c977824391feb868c7d698beedec867a3122
Calin Rada <calin.rada@yahoo.com>
2011-10-23 20:09:24 +0200

    refs #3 : Added icons to media plugin

--- modules/crMediaGalleryContent/templates/_list_td_tabular.php
+++ modules/crMediaGalleryContent/templates/_list_td_tabular.php
@@ -1,14 +1,22 @@
 <td class="sf_admin_foreignkey sf_admin_list_td_name">
   <?php echo $cr_media_gallery_content->getName() ?>
 </td>
-<td class="sf_admin_text sf_admin_list_td_content">
-  <a href="<?php echo '/uploads/galleries/'.$cr_media_gallery_content->getContent() ?>" rel="prettyPhoto[gallery]"><img title="<?php echo $cr_media_gallery_content->getName() ?>" src="<?php echo '/uploads/galleries/'.$cr_media_gallery_content->getContent() ?>" height="35" width="35" /></a>
-</td>
 <td class="sf_admin_foreignkey sf_admin_list_td_gallery_id">
-  <?php echo $cr_media_gallery_content->getGalleryId() ?>
+  <?php echo $cr_media_gallery_content->getCrMediaGallery() ?>
 </td>
 <td class="sf_admin_enum sf_admin_list_td_type">
-  <?php echo $cr_media_gallery_content->getType() ?>
+  <?php
+    $type = $cr_media_gallery_content->getType();
+    switch($type)
+    {
+      case "image": $src = "/crMediaPlugin/images/icons/image.png"; break;
+      case "video": $src = "/crMediaPlugin/images/icons/video.png"; break;
+      case "audio": $src = "/crMediaPlugin/images/icons/audio.png"; break;
+    }
+  ?>
+  <a href="<?php echo '/uploads/galleries/'.$cr_media_gallery_content->getContent() ?>" rel="prettyPhoto[gallery]">
+  <img src="<?php echo $src ?>" width="24" height="24" title="<?php echo __($type) ?>" />
+  </a>
 </td>
 <td class="sf_admin_boolean sf_admin_list_td_is_published">
   <?php echo get_partial('crMediaGalleryContent/list_field_boolean', array('value' => $cr_media_gallery_content->getIsPublished())) ?>
